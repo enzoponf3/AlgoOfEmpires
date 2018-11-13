779c12e319db5e9d621d029d03a680beb45b25db
Nico Garofalo <leiisure98@gmail.com>
2018-11-13 01:39:42 -0300

    Cambios minimos a espadachin para pruebas

--- src/Espadachin.java
+++ src/Espadachin.java
@@ -1,12 +1,12 @@
-public class Espadachin {}
-//    protected int VIDA = 100;
-  //  protected int COSTO = 50;
-/*
-    public Espadachin(int x, int y){
-        super(x,y);
+public class Espadachin extends Unidad{
+   protected int VIDA = 100;
+   protected int COSTO = 50;
+
+
+    public Espadachin(){
         this.vida = VIDA;
         this.costo = COSTO;
-        this.estado = new EstadoEspadachinLibre();
+        this.estado = new EstadoLibre();
     }
     public int getVida() {
         return this.vida;
@@ -16,9 +16,4 @@ public class Espadachin {}
         return this.costo;
     }
 
-    public void atacar(Unidad unidad){
-        this.estado.estaOcupado();
-
-    }
 }
-*/

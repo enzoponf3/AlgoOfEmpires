5c72bc29401a034f10191a2d04786b6b73d5c1a5
Nico Garofalo <leiisure98@gmail.com>
2018-11-14 23:49:45 -0300

    Posicion editado

--- src/Posicion.java
+++ src/Posicion.java
@@ -3,47 +3,17 @@ public class Posicion {
     private int horizontal;
     private int vertical;
 
-
-
     Posicion(int horizontal,int vertical) {
         this.horizontal = horizontal;
         this.vertical = vertical;
     }
 
-    public int getCoordenadaHorizontal(){
-        return this.horizontal;
-    }
-
-    public int getCoordenadaVertical(){
-        return this.vertical;
-    }
-
-
     public boolean igualA(Posicion unaposicion){
         return ( (this.horizontal == unaposicion.horizontal) && (this.vertical == unaposicion.vertical) );
-    } //Aca estoy rompiendo el encapsulamiento? CAMBIAR NOMBRE; ES BOOL
+    }
 
     public boolean estaDentroDelMapa(int limite1, int limite2) {
         return ( (this.horizontal >= 0 ) && (this.horizontal < limite1) && (this.vertical >= 0) && (this.vertical < limite2) );
     } // Decidir si parto de 0x0 o de 1x1. Y si hago el pasaje aca adentro.
-
-    public void incrementarCoordenadaHorizontal() {
-        //Le tiene que pedir lo slimites a la fahcada
-
-        this.horizontal += 1;
-    }
-
-    public void decrementarCoordenadaHorizontal() {
-        this.horizontal -= 1;
-    }
-
-    public void incrementarCoordenadavertical() {
-        this.vertical += 1;
-    }
-
-    public void decrementarCoordenadaVertical() {
-        this.vertical -= 1;
-    }
-
 
 }

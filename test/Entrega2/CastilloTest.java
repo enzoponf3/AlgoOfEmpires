10fae3da3c222270b9e749f86262e37dd2abdc81
Nico Garofalo <leiisure98@gmail.com>
2018-11-18 23:31:31 -0300

    Mas tests agregados

--- test/Entrega2/CastilloTest.java
+++ test/Entrega2/CastilloTest.java
@@ -4,6 +4,7 @@ import Modelo.Edificios.Castillo;
 import Modelo.Exceptions.EdificioConVidaAlMaximoException;
 import Modelo.Exceptions.EdificioDestruidoException;
 import Modelo.Exceptions.EdificioEnReparacionException;
+import Modelo.Unidades.ArmaDeAsedio;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -80,25 +81,25 @@ public class CastilloTest {
         castillo.reparar();
     }
 
-
-
     @Test (expected = EdificioDestruidoException.class)
     public void volverAEstadoOriginalTrasDestruirCastillo() {
         castillo.reducirVida(1000);
         castillo.volverAEstadoOriginal();
     }
-    //    @Test
-//    public void atacarUnaUnidad() {
-//
-//    }
-//
-//    @Test
-//    public void atacarUnEdificio() {
-//
-//    }
-//
-//    @Test
-//    public void atacarUnidadesYEdificios() {
-//
-//    }
+
+    @Test
+    public void castilloCrearArmaAsedioExitosamente(){
+        ArmaDeAsedio armaDeAsedio = castillo.crearArmaDeAsedio();
+        Assert.assertNotNull(armaDeAsedio);
+    }
+
+    @Test
+    public void castilloEnReparacionCreaArmaDeAsedio(){
+        castillo.reducirVida(50);
+        castillo.reparar();
+        ArmaDeAsedio armaDeAsedio = castillo.crearArmaDeAsedio();
+        Assert.assertNotNull(armaDeAsedio);
+    }
+    
+
 }
